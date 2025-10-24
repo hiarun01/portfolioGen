80be22ed690bd1e0bc7734b669fe4b8f5ec694ae
hiarun01 <hiarun02@gmail.com>
2025-10-24 12:15:38 +0530

    project file initlize, setup frotend and backend

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import connectDB from "./config/db.js";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
